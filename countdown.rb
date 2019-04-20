87d055bb2083193d62d5bdfb7622fe7ae4328c78
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-20 20:28:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 
 
 def countdown(seconds)
-  counter=seconds
-  while counter>0 
+  while seconds>0 
   puts "#{seconds} SECOND(S)!"
-  counter-=1 
+  seconds-=1 
 end
 end
 puts "HAPPY NEW YEAR!"
