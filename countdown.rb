610852d3b7ca0f502d026ed7af41f2c7245b2059
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-20 20:40:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ def countdown(seconds)
   while seconds>0 
   puts "#{seconds} SECOND(S)!"
   seconds-=1
-  puts "HAPPY NEW YEAR!"
 end
+  puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds)
